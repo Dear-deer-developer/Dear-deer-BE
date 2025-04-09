8783fa70d829a9f2256ea3d2012fe3cbca3da539
JeonHeena <heena0727@gmail.com>
2025-04-09 16:16:14 +0900

    feat/#6/appmodule 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,12 @@ import { AppService } from './app.service';
 
 import { FirebaseAdminModule } from './firebase/firebase-admin.module';
 import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [FirebaseAdminModule, UsersModule, AuthModule, PrismaModule],
-  controllers: [AppController, AuthController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
