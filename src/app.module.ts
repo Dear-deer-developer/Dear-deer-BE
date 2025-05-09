5ee3c7ef8676376a25d28ce7d351996c1ac8f88a
JeonHeena <heena0727@gmail.com>
2025-05-09 16:13:21 +0900

    feat/#11/HttpModule 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    HttpModule,
   ],
   controllers: [AppController],
   providers: [AppService],
