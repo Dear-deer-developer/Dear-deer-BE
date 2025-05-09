fd76d86545b410ef3212e467d6d26368dfdf0f3a
JeonHeena <heena0727@gmail.com>
2025-05-09 16:09:10 +0900

    feat/#11/kakaoLogin auth.service.ts파일

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { FirebaseAdminService } from '../firebase/firebase-admin.service';
 import { UsersService } from '../users/users.service';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly firebaseAdmin: FirebaseAdminService,
     private readonly usersService: UsersService,
+    private readonly httpService: HttpService, //kakao API 호출용
   ) {}
 
   async authenticate(idToken: string) {
     try {
       const decoded = await this.firebaseAdmin.verifyToken(idToken);
-
       const providerId = decoded.uid;
       const nickname = decoded.name || '익명';
       const zipCode = 0;
@@ -32,6 +34,36 @@ export class AuthService {
       return user;
     } catch (err) {
       throw new UnauthorizedException('Invalid Firebase Token');
+    }
+  }
+
+  // Kakao Access Token -> Firebase Custom Token 발급
+  async getFirebaseCustomToken(accessToken: string): Promise<string> {
+    try {
+      const { data } = await lastValueFrom(
+        this.httpService.get('https://kapi.kakao.com/v2/user/me', {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }),
+      );
+
+      const kakaoId = 'kakao:${data.id}';
+      const nickname = data.properties?.nickname || '익명';
+      const zipCode = 0;
+
+      let user = await this.usersService.findByProviderId(kakaoId);
+
+      if (!user) {
+        user = await this.usersService.create({
+          providerId: kakaoId,
+          nickname,
+          zipCode: zipCode,
+        });
+      }
+
+      const firebaseToken = await this.firebaseAdmin.createCustomToken(kakaoId);
+      return firebaseToken;
+    } catch (err) {
+      throw new UnauthorizedException('Invalid Kakao Access Token');
     }
   }
 }
