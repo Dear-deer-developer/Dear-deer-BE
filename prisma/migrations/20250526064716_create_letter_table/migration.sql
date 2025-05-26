f17ca3f1cc8362afd3fd1731ad9a7eba257aeb17
이건우 <yja20850@naver.com>
2025-05-26 15:58:38 +0900

    feat/#10/letter 테이블 생성

---
+++ prisma/migrations/20250526064716_create_letter_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "LetterStatus" AS ENUM ('writing', 'sent', 'received');
+
+-- CreateTable
+CREATE TABLE "letters" (
+    "id" SERIAL NOT NULL,
+    "sender_id" INTEGER NOT NULL,
+    "receiver_id" INTEGER,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "image_url" TEXT,
+    "status" "LetterStatus" NOT NULL,
+    "sent_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "letters_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "letters" ADD CONSTRAINT "letters_sender_id_fkey" FOREIGN KEY ("sender_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "letters" ADD CONSTRAINT "letters_receiver_id_fkey" FOREIGN KEY ("receiver_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
