0ffd840c6504e741b52cadeaaa4fe303e4cf2767
이건우 <yja20850@naver.com>
2025-05-26 19:59:38 +0900

    feat/#10/class-validator 설치

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // string → number 등 자동 변환
+      whitelist: true, // DTO에 명시되지 않은 값 제거
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
