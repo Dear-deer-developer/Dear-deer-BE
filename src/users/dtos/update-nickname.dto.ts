c7105c38004e182b5083c9f19187a9ea38497e8a
JeonHeena <heena0727@gmail.com>
2025-07-04 16:18:49 +0900

    update-nickname dto 닉네임설정 변경

--- src/users/dtos/update-nickname.dto.ts
+++ src/users/dtos/update-nickname.dto.ts
@@ -1,12 +1,16 @@
-import { IsString, Length } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateNicknameDto {
   @ApiProperty({
     example: '디어디어',
-    description: '등록할 닉네임 (몇자인지 미정, 한글/영어/숫자사용)',
+    description:
+      '닉네임 (2~8자, 한글/영어/숫자만 허용, 공백/이모티콘/특수문자 불가)',
   })
   @IsString()
-  @Length(2, 12) //미정
+  @Matches(/^[가-힣a-zA-Z0-9]{2,8}$/, {
+    message:
+      '닉네임은 2~8자의 한글, 영어, 숫자만 허용되며, 공백/이모티콘/특수문자는 허용되지 않습니다.',
+  })
   nickname: string;
 }
