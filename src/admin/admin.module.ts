378af12d98066dce26f0d381b10129ea6011b002
JeonHeena <heena0727@gmail.com>
2025-08-26 19:39:23 +0900

    feat/#24/의존성 주입(Nest DI)

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminGuard } from './admin.guard';
-
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 @Module({
+  imports: [AuthModule, UsersModule],
   controllers: [AdminController],
   providers: [AdminGuard],
 })
