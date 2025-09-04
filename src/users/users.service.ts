fd04f6387fd4e0c1351fe727e6f3e40988603ea7
JeonHeena <heena0727@gmail.com>
2025-09-04 11:55:00 +0900

    feat/#34/사용자 정보 조회

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -28,4 +28,12 @@ export class UsersService {
 
     return this.usersRepository.updateNickname(userId, nickname, zipCode);
   }
+  async getUserInfoByProviderId(providerId: string) {
+    const user = await this.usersRepository.findByProviderId(providerId);
+
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+    return user;
+  }
 }
--- src/users/users.swagger.ts
+++ src/users/users.swagger.ts
@@ -6,7 +6,41 @@ import {
 } from '@nestjs/swagger';
 import { applyDecorators } from '@nestjs/common';
 import { UpdateNicknameDto } from './dtos/update-nickname.dto';
+import { zip } from 'rxjs';
 
+export function SwaggerGetUser() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '사용자 정보 조회',
+      description: '현재 로그인된 사용자의 상세 정보를 조회합니다.',
+    }),
+    ApiResponse({
+      status: 200,
+      description: '사용자 정보 조회 성공',
+      schema: {
+        example: {
+          id: 1,
+          providerId: '123456789',
+          nickname: '디어디어',
+          zipCode: 10001,
+          createdAt: '2025-07-01T00:00:00.000Z',
+        },
+      },
+    }),
+    ApiResponse({
+      status: 404,
+      description: '사용자를 찾을 수 없습니다.',
+      schema: {
+        example: {
+          statusCode: 404,
+          message: '사용자를 찾을 수 없습니다.',
+          error: 'Not Found',
+        },
+      },
+    }),
+    ApiBearerAuth(),
+  );
+}
 export function SwaggerUpdateNickname() {
   return applyDecorators(
     ApiOperation({
