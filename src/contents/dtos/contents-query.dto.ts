5523dbda0e39f0f8805c2bf614c1fe1fd13e6999
JeonHeena <heena0727@gmail.com>
2025-09-10 18:50:21 +0900

    feat/#32/QueryDto 생성

--- src/contents/dtos/contents-query.dto.ts
+++ src/contents/dtos/contents-query.dto.ts
@@ -1,20 +1,20 @@
-import { IsOptional, IsNumberString} from 'class-validator';
+import { IsOptional, IsNumberString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ContentsQueryDto {
-    @ApiProperty({
-        description: '메인 카테고리 ID',
-        required: false,
-    }),
-    @IsOptional()
-    @IsNumberString()
-    mainCategoryId?: string;
+  @ApiProperty({
+    description: '메인 카테고리 ID',
+    required: false,
+  })
+  @IsOptional()
+  @IsNumberString()
+  mainCategoryId?: string;
 
-    @ApiProperty({
-        description: '서브 카테고리 ID',
-        required: false,
-    })
-    @IsOptional()
-    @IsNumberString()
-    subCategoryId?: string;
+  @ApiProperty({
+    description: '서브 카테고리 ID',
+    required: false,
+  })
+  @IsOptional()
+  @IsNumberString()
+  subCategoryId?: string;
 }
