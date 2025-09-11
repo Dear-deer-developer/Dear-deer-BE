6cc2c10d8e4192fb529a8c328b5f7c41e53649fb
JeonHeena <heena0727@gmail.com>
2025-09-11 15:21:30 +0900

    feat/#32/service, controller 추가 : 카테고리별 조회

--- src/contents/content.controller.ts
+++ src/contents/content.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiContent } from './content.swagger';
+import { ContentsQueryDto } from './dtos/contents-query.dto';
 
 @ApiTags('contents')
 @Controller('contents')
@@ -11,8 +12,8 @@ export class ContentController {
   /** 콘텐츠 리스트 조회 (카테고리별 필터링) */
   @Get()
   @ApiContent.findAll()
-  async findAll() {
-    return this.contentService.findAllPublishedContents();
+  async findAll(@Query() query: ContentsQueryDto) {
+    return this.contentService.findAllPublishedContents(query);
   }
 
   /** 특정 콘텐츠 상세 조회 */
