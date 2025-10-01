29144645c33cb3cec255206ee305b30087e30f72
이건우 <yja20850@naver.com>
2025-10-01 20:23:01 +0900

    feat/#39/userGift 테이블 및 giftSlot 관련 테이블 생성

--- src/common/enums/gift-category.enum.ts
+++ src/common/enums/gift-category.enum.ts
@@ -4,7 +4,11 @@ export const GiftCategoryValue = {
   ORNAMENT: 'ORNAMENT',
   STAR: 'STAR',
   ELECTRIC_BULB: 'ELECTRIC_BULB',
-  INTERIOR: 'INTERIOR',
+  WALLPAPER: 'WALLPAPER',
+  GARLAND: 'GARLAND',
+  CARPET: 'CARPET',
+  TRAIN: 'TRAIN',
+  GIFTBOX: 'GIFTBOX',
   ANIMAL: 'ANIMAL',
 } as const;
 export type GiftCategory = ValueOf<typeof GiftCategoryValue>;
