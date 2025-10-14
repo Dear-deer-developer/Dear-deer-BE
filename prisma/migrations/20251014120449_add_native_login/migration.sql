5ee7c611d71440017a384d167728fd5b7b651558
이건우 <yja20850@naver.com>
2025-10-15 02:20:53 +0900

    feat/#44/자체로그인 기능 추가로 인한  user 테이블 수정

---
+++ prisma/migrations/20251014120449_add_native_login/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateEnum
+CREATE TYPE "LoginType" AS ENUM ('NATIVE', 'SOCIAL');
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "email" TEXT,
+ADD COLUMN     "hashed_password" TEXT,
+ADD COLUMN     "login_type" "LoginType" NOT NULL DEFAULT 'NATIVE',
+ALTER COLUMN "provider_id" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_tokens_user_id_key" ON "refresh_tokens"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
