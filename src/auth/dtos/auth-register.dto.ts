5d4b36bc4a5f1263246fe9192665cd1c6cf2b493
이건우 <yja20850@naver.com>
2025-10-15 15:05:26 +0900

    feat/#44/회원가입 validate dto 수정사항 스웨거에도 추가

--- src/auth/dtos/auth-register.dto.ts
+++ src/auth/dtos/auth-register.dto.ts
@@ -16,7 +16,11 @@ export class AuthRegisterDto {
   @IsNotEmpty({ message: '이메일은 필수 입력 항목입니다.' })
   email: string;
 
-  @ApiProperty({ example: 'qlalfqjsgh1@', description: '비밀번호' })
+  @ApiProperty({
+    example: 'qlalfqjsgh1@',
+    description:
+      '비밀번호는 최소 8자, 최대 20자, 영문, 숫자, 특수문자를 모두 포함해야 합니다.',
+  })
   @IsString({ message: '비밀번호는 문자열이어야 합니다.' })
   @IsNotEmpty({ message: '비밀번호는 필수 입력 항목입니다.' })
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/, {
@@ -25,7 +29,11 @@ export class AuthRegisterDto {
   })
   password: string;
 
-  @ApiProperty({ example: '루돌이', description: '닉네임' })
+  @ApiProperty({
+    example: '루돌이',
+    description:
+      '닉네임은 2~8자의 한글, 영어, 숫자만 사용 가능하며, 공백, 특수문자, 이모티콘은 허용되지 않습니다.',
+  })
   @IsString({ message: '닉네임은 문자열이어야 합니다.' })
   @IsNotEmpty({ message: '닉네임은 필수 입력 항목입니다.' })
   @Matches(/^[가-힣a-zA-Z0-9]{2,8}$/, {
