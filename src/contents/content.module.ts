640f9c4797d0f897054ecff09b1df2f9956327b5
JeonHeena <heena0727@gmail.com>
2025-11-18 15:32:30 +0900

    docs/#42/content-admin 컨트롤러 파일명 변경

--- src/contents/content.module.ts
+++ src/contents/content.module.ts
@@ -4,7 +4,7 @@ import { ContentService } from './content.service';
 import { ContentRepository } from './content.repository';
 import { S3Module } from 'src/s3/s3.module';
 import { UsersModule } from 'src/users/users.module';
-import { ContentAdminController } from './content.admin.controller';
+import { ContentAdminController } from './content-admin.controller';
 import { ScrapModule } from 'src/scrap/scrap.module';
 
 @Module({
