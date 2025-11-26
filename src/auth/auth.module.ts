d15b03e70d39e958ac71e31f0ac1517b044b06e0
이건우 <yja20850@naver.com>
2025-11-26 16:40:27 +0900

    feat/#83/순환의존성 에러 해결

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
@@ -26,7 +26,7 @@ import { ReportModule } from 'src/report/report.module';
     UsersModule,
     HttpModule,
     FirebaseAdminModule,
-    ReportModule,
+    forwardRef(() => ReportModule),
   ],
   providers: [
     AuthService,
