8fe218cc9ce0b89383e028e206fc5f81866911d4
이건우 <yja20850@naver.com>
2025-11-26 16:39:26 +0900

    feat/#83/report와 ban 테이블 스키마 생성

---
+++ prisma/migrations/20251126055427_create_report_and_ban_table/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "ReportReason" AS ENUM ('IMPERSONATION_FRAUD', 'SEXUAL_CONTENT', 'SPAM_AND_PHISHING', 'ABUSIVE_LANGUAGE', 'COMMERCIAL_AD', 'POLITICAL_PROPAGANDA');
+
+-- CreateTable
+CREATE TABLE "reports" (
+    "id" SERIAL NOT NULL,
+    "reporter_id" INTEGER NOT NULL,
+    "reported_user_id" INTEGER NOT NULL,
+    "letter_id" INTEGER,
+    "reason" "ReportReason" NOT NULL,
+    "is_resolved" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "reports_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "blocks" (
+    "id" SERIAL NOT NULL,
+    "blocker_id" INTEGER NOT NULL,
+    "blocked_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "blocks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bans" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "reason" TEXT NOT NULL,
+    "banned_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "bans_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "blocks_blocker_id_blocked_id_key" ON "blocks"("blocker_id", "blocked_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bans_user_id_key" ON "bans"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_reporter_id_fkey" FOREIGN KEY ("reporter_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_reported_user_id_fkey" FOREIGN KEY ("reported_user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_letter_id_fkey" FOREIGN KEY ("letter_id") REFERENCES "letters"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "blocks" ADD CONSTRAINT "blocks_blocker_id_fkey" FOREIGN KEY ("blocker_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "blocks" ADD CONSTRAINT "blocks_blocked_id_fkey" FOREIGN KEY ("blocked_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bans" ADD CONSTRAINT "bans_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
