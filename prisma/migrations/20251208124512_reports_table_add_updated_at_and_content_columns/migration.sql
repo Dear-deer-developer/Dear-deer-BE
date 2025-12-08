68c0da47f76256552279e8d8d37c579018cd537b
이건우 <yja20850@naver.com>
2025-12-08 21:46:14 +0900

    setting/#87/마이그레이션 파일 수정

---
+++ prisma/migrations/20251208124512_reports_table_add_updated_at_and_content_columns/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Added the required column `content` to the `reports` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `reports` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "reports" ADD COLUMN     "content" TEXT NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
